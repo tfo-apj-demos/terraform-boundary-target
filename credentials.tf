03cb9fafc528d0bcf8aec1f24836ed0bcb852fdd
Aaron Evans <aarone@outlook.com.au>
2024-02-12 15:40:34 +1100

    updates

--- credentials.tf
+++ credentials.tf
@@ -13,10 +13,7 @@ resource "vault_token" "this" {
 }
 
 resource "boundary_credential_store_vault" "this" {
-  depends_on  = [ 
-    boundary_role.this 
-  ]
-  name        = "HCP Vault"
+    name        = "HCP Vault"
   address     = var.vault_address
   token       = vault_token.this.client_token
   namespace   = "admin/tfo-apj-demos"
@@ -27,7 +24,7 @@ resource "boundary_credential_library_vault" "ldap_creds" {
   count               = var.ldap_credential_library ? 1 : 0
   name                = "ldap_creds"
   description         = "Dynamic LDAP credentials"
-  credential_store_id = boundary_credential_store_vault.foo.id
+  credential_store_id = boundary_credential_store_vault.this.id
   path                = "ldap/creds/vault_ldap_dynamic_demo_role"
   http_method         = "GET"
 }
