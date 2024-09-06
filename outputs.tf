ae56767105854cb90e4604f769f2e9c2ec9205fb
Aaron Evans <aarone@outlook.com.au>
2024-09-06 15:44:32 +1000

    refactor: Update outputs.tf to include existing_ssh_credential_library_ids and service_keys

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,14 @@ output "host_catalog_id" {
 output "host_ids" {
   value = [for v in boundary_host_static.this : v.id]
 }
+
+output "existing_ssh_credential_library_ids" {
+  value = var.existing_ssh_credential_library_ids
+}
+
+output "service_keys" {
+  value = [for service in local.service_by_credential_path :
+    element(split("/", service.credential_path), length(split("/", service.credential_path)) - 1)
+  ]
+}
+
