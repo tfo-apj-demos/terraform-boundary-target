1e01925e923bb934236a2145926b3ae21345a50e
Aaron Evans <aarone@outlook.com.au>
2024-09-09 12:40:12 +1000

    refactor: Update boundary_target resource in main.tf for improved readability and naming consistency

--- main.tf
+++ main.tf
@@ -95,18 +95,17 @@ resource "boundary_credential_library_vault_ssh_certificate" "this" {
 
 # Boundary target definition
 resource "boundary_target" "this" {
-  for_each = { for service in local.service_by_credential_path :
-    element(split("/", service.credential_path), length(split("/", service.credential_path)) - 1) => service
+  for_each = { for service in var.services :
+    service.name => service  # Use service.name directly as the key
   }
 
-  name         = "${each.key}-${var.hostname_prefix}"
+  name         = "${var.hostname_prefix}-access"  # Use the service.name for the key in the target name
   type         = each.value.type
   default_port = each.value.port
   scope_id     = data.boundary_scope.project.id
 
   host_source_ids = [boundary_host_set_static.this.id]
 
-  # Conditional logic to reference existing SSH credential library or create a new one
   injected_application_credential_source_ids = each.value.type == "ssh" ? (
     contains(keys(var.existing_ssh_credential_library_ids), each.key)
       ? [var.existing_ssh_credential_library_ids[each.key]]
@@ -115,3 +114,4 @@ resource "boundary_target" "this" {
 
   ingress_worker_filter = "\"vmware\" in \"/tags/platform\""
 }
+
