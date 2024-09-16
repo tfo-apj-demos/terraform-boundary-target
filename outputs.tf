bde44ab1a7ed01aa07323cb006c334f73e23930b
Aaron Evans <aarone@outlook.com.au>
2024-09-16 22:31:35 +1000

    Remove unused output variables from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,3 @@
-output "existing_ssh_credential_library_ids" {
-  value = var.existing_ssh_credential_library_ids
-}
-
-output "service_keys" {
-  value = [for service in local.service_by_credential_path :
-    element(split("/", service.credential_path), length(split("/", service.credential_path)) - 1)
-  ]
-}
-
-output "host_catalog_id" {
-  description = "The ID of the host catalog, either passed or created."
-  value       = var.host_catalog_id != null ? var.host_catalog_id : boundary_host_catalog_static.this.id
-}
-
-output "host_ids" {
-  value = [for host in boundary_host_static.this : host.id]
-}
-
 output "tcp_with_creds_targets" {
   value = boundary_target.tcp_with_creds
 }
@@ -67,3 +48,4 @@ output "target_map_debug" {
   description = "Mapping of Boundary targets to SSH credentials"
   value       = local.target_map
 }
+
